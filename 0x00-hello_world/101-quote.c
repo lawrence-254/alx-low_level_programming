261309b9fe3fd07c6c460284d7fe0df1452ba63a
lawrence-254 <lodhiambo26@gmail.com>
2023-02-16 07:47:48 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints without printf and puts
+ * Return 1 (when successfull)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
