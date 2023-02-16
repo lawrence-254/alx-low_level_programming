8d33324fe3324364e9e0cfad22ff261f094c799a
lawrence-254 <lodhiambo26@gmail.com>
2023-02-16 06:38:16 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - gateway to run the code
+ * return : 0 (when no code to run)-
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
