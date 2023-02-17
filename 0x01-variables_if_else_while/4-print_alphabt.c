f85056abfbc2554cc6ef38d87505c9a0608dcd0a
lawrence-254 <lodhiambo26@gmail.com>
2023-02-17 13:22:56 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char c;
 
-	char c = 'a';
+	c = 'a';
 
 	while (c <= 'z')
 	{
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - c programme entry point;
+ *
+ * Return: 0 validates your code
+ */
+int main(void)
+{
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i);
+		putchar('\n);
+	}
+	return (0);
+}
