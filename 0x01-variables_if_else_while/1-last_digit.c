9d16dfc247678d3d6cf503c6f37e35310be2efd2
lawrence-254 <lodhiambo26@gmail.com>
2023-02-19 11:58:02 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int n;
+	int n, m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int m = n % 10;
+	m = n % 10;
 
 	if (m > 5)
 	{
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,8 +10,8 @@ int main(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		putchar(i);
-		putchar('\n');
+		printf("%d", i);
+		printf('\n');
 	}
 	return (0);
 }
