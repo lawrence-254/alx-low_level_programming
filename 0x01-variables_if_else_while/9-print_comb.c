980e4dccd3635d9883e0453392dc93d0fb93aeb1
lawrence-254 <lodhiambo26@gmail.com>
2023-02-20 12:53:48 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - c gateway
  *
@@ -7,10 +9,10 @@
 int main(void)
 {
 	int i;
-	for (i = 0; i < 100; i++)
+	for (i = '0'; i < '9'; i++)
 	{
-		putchar(i + '0');
-		if (i < 99)
+		putchar(i);
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
