bb2be7dd026dd3a056fe98d968440cd31ff423bd
lawrence-254 <lodhiambo26@gmail.com>
2023-02-21 10:57:39 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,14 @@
  */
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
 	_putchar('h');
-	_putchar('e');
-	_putchar('l');
-	_putchar('l');
-	_putchar('o');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
