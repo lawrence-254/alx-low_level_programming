ee048f7bc6f715d1ae72d7236f00009c2a725614
lawrence-254 <lodhiambo26@gmail.com>
2023-02-24 11:30:32 -0800

    8

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - does not print 2 and 4
+ * return: void
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
