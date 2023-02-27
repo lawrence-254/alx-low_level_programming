4877c2b111cc5ba4c3c59a3ad6f32a525a6298ae
lawrence-254 <lodhiambo26@gmail.com>
2023-02-27 11:23:23 -0800

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -23,4 +23,10 @@ void puts_half(char *str)
 	}
 	else if (len % 2)
 	{
-		for (n = (len - 1) / 2; n
+		for (n = (len - 1) / 2; n < len - 1; i++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
