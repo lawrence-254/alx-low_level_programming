232783386afb2105be28f772a212d56e60c8dc1e
lawrence-254 <lodhiambo26@gmail.com>
2023-02-27 08:19:45 -0800

    ./0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes the value n
+ * and set it as a pointer with the value 98
+ * @n: the input in question
+ * Return: n to show the value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,4 +15,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
+#endif
