8a0569bf2a5b08d419f2c08ac0dd503219aa0370
lawrence-254 <lodhiambo26@gmail.com>
2023-03-06 09:24:41 -0800

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	int i = 0;
 
 	for (; n > 0; i++)
 	{
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,7 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-	unsigned char *to = dest, *s = src;
-
+	int i;
 	for (i = 0; i < n; i++)
 	{
 		to[i] = s[i];
