1ce54c3334f2f4ce97a7546b5a656bd421c41114
lawrence-254 <lodhiambo26@gmail.com>
2023-03-06 09:30:25 -0800

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
-	for (i = 0; i < n; i++)
+	int i = 0;
+
+	for (; n > 0; i++)
 	{
-		to[i] = s[i];
+		dest[i] = src[i];
+		n--;
 	}
-	return (to);
+	return (dest);
 }
