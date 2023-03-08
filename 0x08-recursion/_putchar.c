ca9867adc379eb4e0eb843215a7d985e46038347
lawrence-254 <lodhiambo26@gmail.com>
2023-03-08 00:39:01 -0800

    putchar

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes character c to stdout
+ * @c: the character in question
+ * Return: 1 when successfull, -1 when unsuccessfull
+ */
+char _putchar(char c)
+{
+return(write(1, &c, 1));
+}
