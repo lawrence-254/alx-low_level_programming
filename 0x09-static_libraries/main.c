875634a255f2eb82e49d25f40869dc089976b983
lawrence-254 <lodhiambo26@gmail.com>
2023-03-10 11:29:04 -0800

    0 update

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,6 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - returns strings
+ * Return: 0 on success
+ */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }
