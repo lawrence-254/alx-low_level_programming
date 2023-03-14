b540868a95ed5a79a83e92edf64ba4ef3d4da8bb
lawrence-254 <lodhiambo26@gmail.com>
2023-03-14 03:04:56 -0700

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * **alloc_grid - a function that returns a pointer to a 2d array of integers
+ * @height: height of array
+ * @width: width ////
+ * Return: the grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(grid[i]);
+			}
+			free(grid[i]);
+			return (NULL);
+		}
+	}
+	for (i = 0; j < width; j++)
+		grid[i][j] = 0;
+	return (grid);
+}
