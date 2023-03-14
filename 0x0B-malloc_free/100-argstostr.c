ef3b76b41e3a6898bea0ae3f2e8863f4beaecdaa
lawrence-254 <lodhiambo26@gmail.com>
2023-03-14 03:41:24 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *argstostr - a function that concatenates all the arguments of your program
+ * @ac: accepts grid width
+ * @av: ////// height
+ * Return: arguments line by line
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int count = 0, a = 0, b = 0, c = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (a < ac)
+	{
+		b = 0;
+		while (av[a][b] != '\0')
+		{
+			count++;
+			b++;
+		}
+		a++;
+	}
+	count = count + ac + 1;
+	str = malloc(sizeof(char) * count);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
+	}
+	return (str);
+}
