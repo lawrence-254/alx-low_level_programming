7d1639cb7a17eceb0eb0c87f16d0b3716bbd9cde
lawrence-254 <lodhiambo26@gmail.com>
2023-04-06 03:29:38 -0700

    index2

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-	else
-	{
-		*n = ((1 << index) | *n);
-		return (1);
-	}
+	*n = ((1 << index) | *n);
+	return (1);
 }
