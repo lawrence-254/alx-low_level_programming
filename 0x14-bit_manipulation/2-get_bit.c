ed53d331f0bfe868903d0ae8efd9d9afe1335a08
lawrence-254 <lodhiambo26@gmail.com>
2023-04-06 03:06:59 -0700

    index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,8 +8,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int result;
-
-	result = (n >> index) & 1;
-	return (result);
+	if (index > 63)
+		return (-1);
+	else
+		return ((n >> index) & 1);
 }
