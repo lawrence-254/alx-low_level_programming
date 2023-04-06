2169d4a0a5539a640e38235a3c6d01ca5a3b2625
lawrence-254 <lodhiambo26@gmail.com>
2023-04-06 02:39:13 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,9 +16,10 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
-		int to_integer = b[i] - '0';
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
 
-		result = 2 * result + to_integer;
+		result = 2 * result + (b[i] - '0');
 	}
 	return (result);
 }
