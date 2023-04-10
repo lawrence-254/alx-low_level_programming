6e42ded3351a6eb5c4ffe558292a9669270cb860
lawrence-254 <lodhiambo26@gmail.com>
2023-04-09 23:19:07 -0700

    a1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create
+ * @text_content: file contents
+ * Returns: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (filename == NULL || (fd == -1 || w == -1))
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
