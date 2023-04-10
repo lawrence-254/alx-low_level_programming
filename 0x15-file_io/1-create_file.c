e63121e59379161ba3e8d02e0bb065287f3ed826
lawrence-254 <lodhiambo26@gmail.com>
2023-04-09 23:45:06 -0700

    corre 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,10 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
+	else
+	{
+		text_content = "";
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 	if ((filename == NULL) || (fd == -1 || w == -1))
