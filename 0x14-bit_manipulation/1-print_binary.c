c9138f728713a9bbab794c52522cbdd7dbaffe02
lawrence-254 <lodhiambo26@gmai.com>
2023-05-04 03:39:14 -0700

    to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_binary - puts a decimal number in binary
+ * @n: number to print in question
+ * Return: a binary number
+ */
+void print_binary(unsigned long int n)
+{
+	int i, size = 0;
+	unsigned long int num;
+
+	if (!size)
+		_putchar('0');
+	for (i = 63; i >= 0; i--)
+	{
+		num = n >> i;
+
+		if (num & 1)
+		{
+			_putchar('1');
+			size++;
+		}
+		else if (size)
+			_putchar('0');
+	}
+}
