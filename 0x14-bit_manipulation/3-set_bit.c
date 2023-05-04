4d54c48127285ebe683839cfa7b151f3b9a424ff
lawrence-254 <lodhiambo26@gmai.com>
2023-05-04 04:01:42 -0700

    set to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_bit - converts value of an index to 1
+ * @n: pointer to the value
+ * @index: index in question
+ * Return: 1 pass else -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n = ((1UL << index) | *n);
+	return (1);
+
+	if (index > 63)
+		return (-1);
+}
