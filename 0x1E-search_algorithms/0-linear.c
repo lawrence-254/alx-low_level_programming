9d08bce56421eb894cb53e5504ccee696be11f42
lawrence-254 <lodhiambo26@gmail.com>
2023-10-09 16:14:04 +0000

    0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+/**
+* linear_search - Linear search algorithm function
+* @array: pointer to the first element of the array to search in
+* @size:number of elements in array
+* @value: value to search for
+* Return: first index where value is located
+*/
+int linear_search(int *array, size_t size, int value)
+{
+
