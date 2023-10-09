130e8df3216239a0309b754fdc2cbb09559810cf
lawrence-254 <lodhiambo26@gmail.com>
2023-10-09 10:04:24 -0700

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,4 +8,18 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
+	int i;
 
+	if (array != NULL)
+	{
+		for (i = 0; i < (int) size; i++)
+		{
+			if (array[i] == value)
+			{
+				return (i);
+			}
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		}
+	}
+	return (-1);
+}
