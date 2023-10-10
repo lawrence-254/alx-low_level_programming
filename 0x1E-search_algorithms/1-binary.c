b66561b8f1d9962b248bb2a286165f4d345c9e52
lawrence-254 <lodhiambo26@gmail.com>
2023-10-10 13:03:49 -0700

    why you not love betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,11 +1,12 @@
 #include "search_algos.h"
 /*
- * binary_search - Performs binary search on an array of sorted integers.
+ * binary_search - searches for a value in a sorted array of integers
  *
  * @array:  pointer to the first element of the array to search in
  * @size:  number of elements in array
  * @value: the value to search for
- * Return: the index where value is located or NULL
+ *
+ * Return: the index where value is located or -1
  */
 int binary_search(int *array, size_t size, int value)
 {
